569bcdc43355444636499f8af074e475c8229cdc
Bruno Sánchez <90653850+BrunoSaPa@users.noreply.github.com>
2024-12-31 17:30:57 -0600

    Post processing stuff

--- src/components/RotatingModel.jsx
+++ src/components/RotatingModel.jsx
@@ -2,19 +2,22 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import { Center } from '@react-three/drei';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
+import { Bloom, EffectComposer, Glitch, DotScreen, Scanline } from '@react-three/postprocessing';
 import * as THREE from 'three';
+import { GlitchMode, BlendFunction } from 'postprocessing'
 
 const RotatingModel = () => {
     const modelRef = useRef();
     const { scene: gltfScene } = useGLTF('/assets/neonflower.glb');
-    const lerpSpeed = 0.04; // Controla la velocidad de la interpolación
-    const [rotationSpeed, setRotationSpeed] = useState(0); // Velocidad de rotación controlada por scroll
-    const [touchStartX, setTouchStartX] = useState(null); // Coordenada inicial del toque
-    const acceleration = 0.0008; // Incremento de velocidad por cada evento de scroll
-    const maxSpeed = 0.3; // Límite máximo de velocidad
+    const lerpSpeed = 0.01;
+    const [rotationSpeed, setRotationSpeed] = useState(0);
+    const [touchStartX, setTouchStartX] = useState(null);
+    const acceleration = 0.0008;
+    const maxSpeed = 0.5;
+    const [pulseIntensity, setPulseIntensity] = useState(0);
+    const [isPulsating, setIsPulsating] = useState(false);
+    const [isNearZero, setIsNearZero] = useState(false);
 
-    // Mapa de colores de destino para cada child
     const colorMap = useRef({
         body001: { current: new THREE.Color(0x679436), target: new THREE.Color(0x679436), emissiveIntensity: 4 },
         boot001: { current: new THREE.Color(0x873f00), target: new THREE.Color(0x873f00), emissiveIntensity: 10 },
@@ -26,47 +29,56 @@ const RotatingModel = () => {
         head002: { current: new THREE.Color(0x000000), target: new THREE.Color(0x000000), emissiveIntensity: 0 }
     });
 
+    const computeFinalIntensity = (child, targetIntensity) => {
+        if (isNearZero) {
+            if (child.name !== 'body002' && child.name !== 'boot002' && child.name !== 'petal002' && child.name !== 'head002') {
+                return THREE.MathUtils.lerp(child.material.emissiveIntensity, 3, lerpSpeed);
+            }
+            return child.material.emissiveIntensity;
+        } else {
+            return THREE.MathUtils.lerp(child.material.emissiveIntensity, targetIntensity, lerpSpeed);
+        }
+    };
+
     useEffect(() => {
         const handleWheel = (event) => {
-            event.preventDefault(); // Evita el scroll de la página
-    
-            // Determina la dirección del scroll
+            event.preventDefault();
+
             const direction = event.deltaY || event.deltaX;
-            
-            // Incrementa la velocidad con sensibilidad ajustada para PC
-            const pcAcceleration = acceleration; // Sensibilidad normal
+
+            const pcAcceleration = acceleration;
             setRotationSpeed((prevSpeed) => {
                 const newSpeed = prevSpeed + direction * pcAcceleration;
                 return THREE.MathUtils.clamp(newSpeed, -maxSpeed, maxSpeed);
             });
         };
-    
+
         const handleTouchStart = (event) => {
             // Almacena la posición inicial del toque
             setTouchStartX(event.touches[0].clientX);
         };
-    
+
         const handleTouchMove = (event) => {
             if (touchStartX === null) return;
-    
+
             const touchEndX = event.touches[0].clientX;
             const direction = touchEndX > touchStartX ? -1 : 1; // Determina la dirección del swipe
-    
+
             // Incrementa la velocidad con mayor sensibilidad para móviles
-            const touchAcceleration = acceleration * 10; // Incrementa la sensibilidad en móviles
+            const touchAcceleration = acceleration * 5; // Incrementa la sensibilidad en móviles
             setRotationSpeed((prevSpeed) => {
                 const newSpeed = prevSpeed + direction * touchAcceleration;
                 return THREE.MathUtils.clamp(newSpeed, -maxSpeed, maxSpeed);
             });
-    
+
             setTouchStartX(touchEndX); // Actualiza la posición inicial para el siguiente movimiento
         };
-    
+
         // Agregar eventos para scroll (PC) y táctiles (móviles)
         window.addEventListener('wheel', handleWheel, { passive: false });
         window.addEventListener('touchstart', handleTouchStart, { passive: false });
         window.addEventListener('touchmove', handleTouchMove, { passive: false });
-    
+
         return () => {
             // Eliminar los eventos al desmontar el componente
             window.removeEventListener('wheel', handleWheel);
@@ -74,15 +86,37 @@ const RotatingModel = () => {
             window.removeEventListener('touchmove', handleTouchMove);
         };
     }, [touchStartX]);
-    
 
-    useFrame(() => {
+    useEffect(() => {
+        // Randomized pulsation logic
+        const triggerPulsation = () => {
+            if (!isNearZero) {
+                setIsPulsating(true);
+                setTimeout(() => {
+                    setIsPulsating(false);
+                }, 2000); // Pulsate for 2 seconds
+            }
+
+            // Schedule next pulsation randomly between 2-7 seconds
+            const delay = Math.random() * 5000 + 2000;
+            setTimeout(triggerPulsation, delay);
+        };
+
+        triggerPulsation();
+    }, [isNearZero]);
+
+    useFrame((state) => {
         if (modelRef.current) {
             // Rotar el modelo
             //modelRef.current.rotation.y -= 0.02;
 
             modelRef.current.rotation.y += rotationSpeed;
             setRotationSpeed((prev) => THREE.MathUtils.lerp(prev, 0, 0.05));
+
+
+            const time = state.clock.getElapsedTime();
+            const sineWave = isPulsating ? Math.sin(time * 5) * 0.5 + 0.5 : 0;
+            setPulseIntensity(sineWave);
 
             // Convertir la rotación Y a grados
             const rotationYInDegrees = THREE.MathUtils.radToDeg(modelRef.current.rotation.y % (2 * Math.PI));
@@ -91,7 +125,9 @@ const RotatingModel = () => {
             const normalizedDegrees = (rotationYInDegrees + 360) % 360;
 
             // Detectar si está cerca de los rangos definidos
-            const isNearZero = normalizedDegrees < 120 || normalizedDegrees > 240;
+            setIsNearZero(normalizedDegrees < 120 || normalizedDegrees > 240);
+
+
 
             // Ajustar colores y emissiveIntensity
             gltfScene.traverse((child) => {
@@ -99,18 +135,13 @@ const RotatingModel = () => {
                     const colorInfo = colorMap.current[child.name];
 
                     if (colorInfo) {
-                        // Si está cerca de 0 o 360 grados, cambiar el color a gris y el emissiveIntensity a 0
-                        const targetColor = isNearZero ? new THREE.Color(0x000000) : colorInfo.target;
-                        const targetEmissiveIntensity = isNearZero ? 0.1 : colorInfo.emissiveIntensity;
-
-                        // Interpolación de color y emissiveIntensity para cada mesh individualmente
+                        const targetIntensity = colorInfo.emissiveIntensity + pulseIntensity * 3;
+                        const targetColor = isNearZero ? ((child.name != 'body002' && child.name != 'head002' && child.name != 'boot002' && child.name != 'petal002') ? new THREE.Color(0x9b9b9b) : colorInfo.target) : colorInfo.target;
+                        const finalIntensity = computeFinalIntensity(child, targetIntensity);
                         colorInfo.current.lerp(targetColor, lerpSpeed);
                         child.material.emissive = colorInfo.current;
                         child.material.color = colorInfo.current;
-
-                        // Suavizar la transición del emissiveIntensity
-                        child.material.emissiveIntensity = THREE.MathUtils.lerp(child.material.emissiveIntensity, targetEmissiveIntensity, lerpSpeed);
-
+                        child.material.emissiveIntensity = finalIntensity;
                         child.material.needsUpdate = true;
                     }
                 }
@@ -160,7 +191,20 @@ const RotatingModel = () => {
                 <primitive ref={modelRef} object={gltfScene} scale={[0.7, 0.7, 0.7]} rotation={[0, Math.PI, 0]} />
             </Center>
             <EffectComposer>
-                <Bloom intensity={1.0} luminanceThreshold={0.5} luminanceSmoothing={0.1} />
+                <Bloom intensity={1} luminanceThreshold={.4} luminanceSmoothing={0.2} />
+                {isNearZero && <Glitch
+                    delay={[0, .5]} // min and max glitch delay
+                    duration={[0.6, 3.0]} // min and max glitch duration
+                    strength={[1, 20]} // min and max glitch strength
+                    mode={GlitchMode.SPORADIC} // glitch mode
+                    ratio={0.85} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+                />}
+
+                <Scanline
+                    blendFunction={BlendFunction.OVERLAY} // blend mode
+                    density={2} // scanline density
+                />
+
             </EffectComposer>
         </>
     );
